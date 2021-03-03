4e4743f6ef98d9b69b3b47fd46f1ae5546fe6d4f
Max <axa.fenixxx@meta.ua>
2021-03-03 12:53:48 +0200

    Move error_2

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,17 +6,36 @@ public class Player : MonoBehaviour {
     List<GameObject> targets;
     [SerializeField]
     float speed;
+    
+    Rigidbody rb;
     void Start()
     {
         targets = new List<GameObject>();
         targets.AddRange(GameObject.FindGameObjectsWithTag("Target"));
+        rb = gameObject.GetComponent<Rigidbody>();
         print(targets.Count);
     }
     void Update()
     {
-        
+        MoveToTarget();
     }
-    void MoveToTarget(IEnumerable<GameObject> targets) {
-
+    void MoveToTarget() {
+        gameObject.transform.LookAt(GetTarget(targets).transform);
+        //rb.MovePosition(transform.position + GetTarget(targets).transform.position * Time.fixedDeltaTime*0.1f);
+        //transform.position += GetTarget(targets).transform.position * Time.deltaTime*speed;
+        Vector3.MoveTowards(transform.position, GetTarget(targets).transform.position, Time.deltaTime * speed);
+        print(GetTarget(targets).name);
+    }
+    GameObject GetTarget(List<GameObject> targets) {
+        int indexTarget = 0;
+        if(indexTarget != targets.Count - 1) {
+            if (transform.position == targets[indexTarget].transform.position) {
+                print("Final destination");
+                indexTarget++;
+                return targets[indexTarget];
+            }
+        }
+        indexTarget = 0;
+        return targets[indexTarget];
     }
 }
