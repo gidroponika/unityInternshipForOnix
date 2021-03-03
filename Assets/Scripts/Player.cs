57b89ff0317f2f445fb43ed38455c06a4892b095
Max <axa.fenixxx@meta.ua>
2021-03-03 16:09:07 +0200

    Final version 0.1

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,38 +4,39 @@ using UnityEngine;
 public class Player : MonoBehaviour {
     [SerializeField]
     List<GameObject> targets;
+
     [SerializeField]
     float speed;
-    
-    Rigidbody rb;
+
+    int indexTarget;
     void Start()
     {
         targets = new List<GameObject>();
         targets.AddRange(GameObject.FindGameObjectsWithTag("Target"));
-        rb = gameObject.GetComponent<Rigidbody>();
-        print(targets.Count);
+        indexTarget = 0;
     }
     void Update()
     {
-        MoveToTarget();
+        MovementToTarget();
     }
-    void MoveToTarget() {
+
+    //method of movement to the target
+    void MovementToTarget() {
         gameObject.transform.LookAt(GetTarget(targets).transform);
-        //rb.MovePosition(transform.position + GetTarget(targets).transform.position * Time.fixedDeltaTime*0.1f);
-        //transform.position += GetTarget(targets).transform.position * Time.deltaTime*speed;
-        Vector3.MoveTowards(transform.position, GetTarget(targets).transform.position, Time.deltaTime * speed);
-        print(GetTarget(targets).name);
+        transform.position = Vector3.MoveTowards(transform.position, 
+                             GetTarget(targets).transform.position, 
+                             Time.deltaTime * speed);
+        
     }
+
+    // method, that sets a new target when the old one is reached
     GameObject GetTarget(List<GameObject> targets) {
-        int indexTarget = 0;
-        if(indexTarget != targets.Count - 1) {
-            if (transform.position == targets[indexTarget].transform.position) {
-                print("Final destination");
-                indexTarget++;
-                return targets[indexTarget];
+        if (gameObject.transform.position == targets[indexTarget].transform.position) {
+            indexTarget++;
+            if (indexTarget == targets.Count) {
+                indexTarget = 0;
             }
         }
-        indexTarget = 0;
         return targets[indexTarget];
     }
 }
