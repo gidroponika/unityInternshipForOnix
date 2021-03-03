f746f52c7ecd811dfd449558615a15de0f349beb
Max <axa.fenixxx@meta.ua>
2021-03-03 12:49:38 +0200

    Move error

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Player : MonoBehaviour {
+    [SerializeField]
+    List<GameObject> targets;
+    [SerializeField]
+    float speed;
+    void Start()
+    {
+        targets = new List<GameObject>();
+        targets.AddRange(GameObject.FindGameObjectsWithTag("Target"));
+        print(targets.Count);
+    }
+    void Update()
+    {
+        
+    }
+    void MoveToTarget(IEnumerable<GameObject> targets) {
+
+    }
+}
